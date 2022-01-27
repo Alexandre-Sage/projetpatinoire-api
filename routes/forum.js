7dd867dd3706878f096100302cae0cb0ae9b4b9d
Alexandre <chorizosurffista@gmail.com>
2022-01-27 12:01:02 +0100

    peut etre le dernier avant longtemp

--- routes/forum.js
+++ routes/forum.js
@@ -16,19 +16,28 @@ router.get("/categories", function(req, res, next){
 })
 module.exports = router;
 
-router.get("/topics", function(req, res, next){
+router.get("/topics/:categoryId", function(req, res, next){
     const dataBase= req.app.locals.db;
-    dataBase.query("SELECT * FROM forumTopics",[], function(err, forumTopics){
+    console.log(req.params.categoryId);
+    const sqlRequestForumCategoriesTopicsRoute= `SELECT * FROM forumTopics
+                                                    WHERE categoryId=?`
+    dataBase.query(sqlRequestForumCategoriesTopicsRoute,[req.params.categoryId], function(err, forumTopics){
         if(err) throw err;
         res.json(forumTopics);
     })
 })
 module.exports = router;
 
-router.get("/posts", function(req, res, next){
+router.get("/posts/:topicId", function(req, res, next){
     const dataBase= req.app.locals.db;
-    dataBase.query("SELECT * FROM forumPosts",[], function(err, forumPosts){
+    const sqlRequestPostsDisplayRoute= `SELECT * FROM forumPosts
+                                        INNER JOIN forumTopics ON forumPosts.topicId
+                                        INNER JOIN (SELECT userName, userId FROM usersProfils) usersProfils ON forumPosts.userId
+                                        WHERE forumPosts.topicId=?
+                                        AND forumTopics.topicId=forumPosts.topicId AND forumPosts.userId=usersProfils.userId`
+    dataBase.query(sqlRequestPostsDisplayRoute, [req.params.topicId], function(err, forumPosts){
         if(err) throw err;
+        console.log(forumPosts);
         res.json(forumPosts);
     })
 })
